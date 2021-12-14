77eced06e99752209b1a7d87b56b68b1899fe8f4
olariudaiana <daiana.olariu519@yahoo.com>
2021-12-14 15:24:53 +0200

    Created pages for admin

---
+++ frontend/FoodOrderingApp/src/app/view-food/view-food.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-food',
+  templateUrl: './view-food.page.html',
+  styleUrls: ['./view-food.page.scss'],
+})
+export class ViewFoodPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
