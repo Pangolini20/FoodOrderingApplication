c3cc984a2d19c5af915d26ad4600adf3d51b74c8
olariudaiana <daiana.olariu519@yahoo.com>
2022-01-03 16:05:54 +0200

    Created the design for page sales

--- frontend/FoodOrderingApp/src/app/sales/sales.page.ts
+++ frontend/FoodOrderingApp/src/app/sales/sales.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MenuController} from "@ionic/angular";
 
 @Component({
   selector: 'app-sales',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SalesPage implements OnInit {
 
-  constructor() { }
+  constructor(private menu:MenuController) { }
 
   ngOnInit() {
+
   }
 
+  _openMenuAdmin(){
+    this.menu.enable(true, 'second');
+    this.menu.open('second');
+  }
 }
