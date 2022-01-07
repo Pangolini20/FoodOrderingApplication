08510b55a7e24445c4a46beeaebbfbc977dad262
olariudaiana <daiana.olariu519@yahoo.com>
2022-01-07 19:45:29 +0200

    Added functionality for shopping cart,buttons for categories

--- frontend/FoodOrderingApp/src/app/discount-codes/discount-codes.page.ts
+++ frontend/FoodOrderingApp/src/app/discount-codes/discount-codes.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {MenuController} from "@ionic/angular";
+import {Router} from "@angular/router";
+import {CartService} from "../Providers/cart.service";
 
 @Component({
   selector: 'app-discount-codes',
@@ -8,9 +10,19 @@ import {MenuController} from "@ionic/angular";
 })
 export class DiscountCodesPage implements OnInit {
 
-  constructor(private menu:MenuController) { }
-
+  constructor(private menu:MenuController, private router: Router, private cartService: CartService) { }
+  cart = [];
+  items = [];
   ngOnInit() {
+    this.items = this.cartService.getProducts();
+    this.cart = this.cartService.getCart();
+  }
+  addToCart(product) {
+    this.cartService.addProduct(product);
+  }
+
+  openCart() {
+    this.router.navigate(['cart']);
   }
   _openMenuClient(){
     this.menu.enable(true, 'main');
