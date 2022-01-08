fe8b1e857776447498e05567c7f9254b0674cd96
olariudaiana <daiana.olariu519@yahoo.com>
2022-01-09 00:13:00 +0200

    Added select restaurant option

--- frontend/FoodOrderingApp/src/app/add-food/add-food.page.ts
+++ frontend/FoodOrderingApp/src/app/add-food/add-food.page.ts
@@ -9,13 +9,45 @@ import {MenuController} from "@ionic/angular";
 export class AddFoodPage implements OnInit {
 
   category:any;
+  restaurant:any;
   constructor(private menu:MenuController) { }
 
   ngOnInit() {
   }
 
+  restaurants=[
+    {
+      id:1,
+      name: 'Restaurant1',
+    },
+    {
+      id:2,
+      name: 'Restaurant2',
+    },
+  ]
+
+  categories=[
+    {
+      id:1,
+      name: 'Burgers',
+    },
+    {
+      id:2,
+      name: 'Pizza',
+    },
+    {
+      id:3,
+      name: 'Sweets',
+    },
+    {
+      id:4,
+      name: 'Drinks',
+    },
+  ]
   _openMenuAdmin(){
     this.menu.enable(true, 'second');
     this.menu.open('second');
   }
+
+
 }
