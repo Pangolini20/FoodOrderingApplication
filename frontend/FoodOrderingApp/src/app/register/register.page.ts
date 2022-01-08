f60208f0134011569669c0b0381e5d1689db78a2
Pangolini20 <dariusopritoiu123@gmail.com>
2022-01-08 18:40:40 +0200

    register and login finished send data to backend and create db entries

--- frontend/FoodOrderingApp/src/app/register/register.page.ts
+++ frontend/FoodOrderingApp/src/app/register/register.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService} from "../Providers/auth.service";
 import {Role} from "../dto/role";
+import {RegisterDetails} from "../dto/register-details";
+import {UserService} from "../services/user.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-register',
@@ -9,18 +11,45 @@ import {Role} from "../dto/role";
 })
 export class RegisterPage implements OnInit {
 
-  roles = [Role.CLIENT.toString(),Role.DELIVERY_GUY.toString(),Role.RESTAURANT_OWNER.toString()];
+  roles = [Role.CLIENT.toString(),Role.RESTAURANT_OWNER.toString(),Role.DELIVERY_GUY.toString()];
   username:string;
   role:Role;
   email: string;
   password: string;
   confirm_password: string;
+  registerData:RegisterDetails;
 
-
-  constructor() { }
-
+  constructor(private userService:UserService, private router:Router) { }
 
   ngOnInit() {
   }
+
+  checkInput() : boolean
+  {
+    if(this.username && this.password && this.role && this.email && this.confirm_password)
+      return true;
+
+    return false;
+
+  }
+
+  register() {
+    this.registerData = new RegisterDetails(this.username, this.password, this.role, this.email);
+    console.log(this.registerData);
+    if (this.checkInput())
+      if (this.password === this.confirm_password) {
+        this.registerData = new RegisterDetails(this.username, this.password, this.role, this.email);
+        this.userService.userRegister(this.registerData).subscribe();
+        this.router.navigateByUrl('/login');
+      }
+      else
+        console.log("Passwords do not match")
+    else
+      console.log("some fields are not completed");
+
+  }
+
+
+
 
 }
